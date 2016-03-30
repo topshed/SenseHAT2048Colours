66ecefcc4fbac659ad4e4147871ad64230a90f4e
Richard Hayler <top_shed@yahoo.com>
2016-03-30 20:14:45 +0100

    added comments

--- col2048.py
+++ col2048.py
@@ -5,15 +5,17 @@ import random
 import time
 import sys
 sh = SenseHat()
-sh.show_message('Starting...',scroll_speed=0.05)
+
+# Check that we can see the SenseHat Joystick
 devices = [InputDevice(fn) for fn in list_devices()]
 for dev in devices:
     print(dev.name)
     if dev.name == "Raspberry Pi Sense HAT Joystick":
         js=dev
-        print('Found Joystick')
+        print('found')
 
-sh.clear()
+sh.clear() # Clear LEDs
+#Define the colours for each 'number' square
 global colour2
 colour2 = [255,0,0]
 global colour4
@@ -29,19 +31,28 @@ colour64 = [255,0,128]
 global colour128
 colour128 = [128,0,255]
 global colour256
-colour256 = [0,0,255]
+colour256 = [225,128,55]
 global colour512
-colour512 = [225,128,55]
+colour512 = [0,0,255]
 global colour1024
 colour1024 = [134,55,10]
 global colour2048
 colour2048 = [255,255,255]
+
+# Dictionary of colours and their corresponding number
+colours = {'2':colour2, '4':colour4,'8':colour8,'16':colour16,
+           '32':colour32,'64':colour64,'128':colour128,'256':colour256,
+           '512':colour512,'1024':colour1024,'2048':colour2048}
+# Print each colour with its number
+for c in sorted(colours,key=lambda k: int(k) if k.isdigit()else float('-inf')):
+    sh.show_message(c,text_colour=[0,0,0],back_colour=colours[c],scroll_speed=0.03)
 
 global score
 score = 0
 r = [255,0,0]
 g = [0,255,0]
 b = [0,0,255]
+# define each 4x4 square and the pixels that comprise it
 global sq00
 sq00 = [(0,0),(0,1),(1,0),(1,1)]
 global sq01
@@ -75,12 +86,7 @@ sq32 = [(6,4),(6,5),(7,4),(7,5)]
 global sq33
 sq33 = [(6,6),(6,7),(7,6),(7,7)]
 
-
-sqs = [sq00,sq01,sq02,sq03,
-       sq10,sq11,sq12,sq13,
-       sq20,sq21,sq22,sq23,
-       sq30,sq31,sq32,sq33]
-
+# Define columns and rows to make lookups easier when moving
 col0 = ['sq03', 'sq02', 'sq01', 'sq00']
 col1 = ['sq13', 'sq12', 'sq11', 'sq10']
 col2 = ['sq23', 'sq22', 'sq21', 'sq20']
@@ -90,7 +96,10 @@ row1 = ['sq01', 'sq11', 'sq21', 'sq31']
 row2 = ['sq02', 'sq12', 'sq22', 'sq32']
 row3 = ['sq03' ,'sq13', 'sq23', 'sq33']
 
-
+# Main dictionary
+# First & second values: x and y of 4x4 squares
+# Third value: colour to be light
+# Fourth value: whether sq has already been merged/ added this movement round
 global grid
 grid = {'sq00':[0,0,'off',False],'sq10':[1,0,'off',False], 'sq20':[2,0,'off',False],'sq30':[3,0,'off',False],
         'sq01':[0,1,'off',False],'sq11':[1,1,'off',False], 'sq21':[2,1,'off',False],'sq31':[3,1,'off',False],
@@ -98,6 +107,7 @@ grid = {'sq00':[0,0,'off',False],'sq10':[1,0,'off',False], 'sq20':[2,0,'off',Fal
         'sq03':[0,3,'off',False],'sq13':[1,3,'off',False], 'sq23':[2,3,'off',False],'sq33':[3,3,'off',False]
         }
 
+# Rules for merging squares -> next colour
 def add_colours(col1):
     if col1 == colour2:
         return(colour4,4)
@@ -120,6 +130,8 @@ def add_colours(col1):
     if col1 == colour1024:
         return(colour2048,2048)
 
+# maps the string (used as key in grid dictionary) to the variable
+# of the same name
 def map_sqs(sqstr):
     if sqstr == 'sq00' :
         return(sq00)
@@ -154,6 +166,7 @@ def map_sqs(sqstr):
     if sqstr == 'sq33' :
         return(sq33)
 
+# Redraw grid by looping through grid dictionary
 def refresh_grid():
     for sq in grid:
         colour = grid[sq][2]
@@ -162,14 +175,20 @@ def refresh_grid():
         else:
             load_sq(map_sqs(sq),colour)
 
+# Load and light up an indiviual 4x4 square with colour
 def load_sq(sqxy,colour):
     for p in range(4):
         #print(sqxy)
         sh.set_pixel(sqxy[p][0],sqxy[p][1],colour)
 
+# Change the value of the colour value of grid dictionary element
 def update_sq(sq, colour):
     grid[sq][2] = colour
 
+# Move everything in grid down
+# We start at the 2nd from bottom row and then work up
+# Do it three times so that a square at the top with no
+# coloured sqs below it will end up at the bottom
 def shift_down():
     global grid
     for g in grid:
@@ -180,6 +199,8 @@ def shift_down():
         UDshift(row0,row1)
     refresh_grid()
 
+# Move everything up
+# Same principle as down but in reverse
 def shift_up():
     global grid
     for g in grid:
@@ -190,6 +211,7 @@ def shift_up():
         UDshift(row3,row2)
     refresh_grid()
 
+# Move everything left
 def shift_left():
     global grid
     for g in grid:
@@ -200,6 +222,7 @@ def shift_left():
         LRshift(col3,col2)
     refresh_grid()
 
+# Move everything right
 def shift_right():
     global grid
     for g in grid:
@@ -210,6 +233,8 @@ def shift_right():
         LRshift(col0,col1)
     refresh_grid()
 
+# Rules for moving left/right. Work through all the elements
+# in a given column, comparing to the next left/right column. Rules:
 def LRshift(firstcol,secondcol):
     global score
     for block in firstcol:
@@ -219,25 +244,25 @@ def LRshift(firstcol,secondcol):
         if colour != 'off':
             for lblock in secondcol:
                 if grid[lblock][1] == y:
+                    # if colour of sq to the left is same and no merge already this round, then merge/add_sq
                     if grid[lblock][2] == colour and grid[lblock][3] == False and grid[block][3] == False:
                         result = add_colours(colour)
                         new_colour = result[0]
                         score= score + result[1]
-                        #print(rblock)
                         load_sq(map_sqs(lblock),new_colour)
                         grid[block][2] = 'off'
                         grid[lblock][2] = new_colour
                         grid[lblock][3] = True
-                    if grid[lblock][2] == colour and grid[lblock][3] == False and grid[block][3] == False:
-                        load_sq(map_sqs(lblock),colour)
-                        #grid[block][2] = 'off'
-                        grid[lblock][2] = colour
+                    # if sq to left is empty (off) then light that sq the same colour
+                    # and set the original sq to off
                     if grid[lblock][2] == 'off':
                         #print(rblock)
                         load_sq(map_sqs(lblock),colour)
                         grid[block][2] = 'off'
                         grid[lblock][2] = colour
 
+# Rules for moving up/down. Work through all the elements
+# in a given column, comparing to the next column above/below.
 def UDshift(firstrow,secondrow):
     global score
     for block in firstrow:
@@ -256,44 +281,48 @@ def UDshift(firstrow,secondrow):
                         grid[block][2] = 'off'
                         grid[lblock][2] = new_colour
                         grid[lblock][3] = True
-                    if grid[lblock][2] == colour and grid[lblock][3] == False and grid[block][3] == False:
-                        load_sq(map_sqs(lblock),colour)
-                        #grid[block][2] = 'off'
-                        grid[lblock][2] = colour
                     if grid[lblock][2] == 'off':
                         #print(rblock)
                         load_sq(map_sqs(lblock),colour)
                         grid[block][2] = 'off'
                         grid[lblock][2] = colour
 
+# Pick a new  free square
 def random_sq():
     empty_sqs = []
+    # Find all the empty squares
     for sq in grid:
         if grid[sq][2] == 'off':
             empty_sqs.append(sq)
+    # If no empty squares, then game over
     if len(empty_sqs) == 0:
         sh.clear()
         for r in range(2):
             sh.show_message("Game Over: score =",text_colour=[255,255,0], back_colour=[0,128,255],scroll_speed=0.05)
-            sh.show_message(str(score),back_colour=[255,0,0],scroll_speed=0.05)
+            sh.show_message(str(score),back_colour=[0,255,0],scroll_speed=0.07)
         sh.clear()
         sys.exit()
+    # Pick a random free square
     chosen_sq = random.choice(empty_sqs)
     return(chosen_sq)
 
+# Add a new '2' sq to a free space and refresh grid
 def add_sq(col):
     new = random_sq()
     update_sq(new,col)
     load_sq(map_sqs(new),col)
     refresh_grid()
 
+# START GAME
+# Add two '2' squares at the start of the game
 add_sq(colour2)
 add_sq(colour2)
 while True:
-
+    # Monitor joystick and capture events
     r,w,x=select([js.fd],[],[],0.01)
     for fd in r:
          for event in js.read():
+             # If it's a 'key' event
               if event.type == ecodes.EV_KEY and event.value==1:
                   if event.code == ecodes.KEY_UP:
                       print("up")
@@ -313,27 +342,3 @@ while True:
                       add_sq(colour2)
                   else:
                       print("enter")
-
-
-
-'''print('displaying random sq' )
-new = random_sq()
-update_sq(new,colour2)
-load_sq(map_sqs(new),colour2)
-new = random_sq()
-update_sq(new,colour2)
-load_sq(map_sqs(new),colour2)
-time.sleep(2)
-print('shifting right')
-shift_right()
-time.sleep(2)
-print('shifting left')
-shift_left()
-time.sleep(2)
-print('shifting up')
-shift_up()
-time.sleep(2)
-print('shifting down')
-shift_down()'''
-
-#load_sq(sq01,colour4)
